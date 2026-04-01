627b85452f7991e4cd9befd2eeeb1ab7c107c435
Abhishek Borana <abhisheksen789bd@gmail.com>
2026-04-02 01:04:24 +0530

    adding fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,20 @@
 import "@/global.css";
+import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
 import { Link } from "expo-router";
-import { Text } from "react-native";
 import { styled } from "nativewind";
+import { Text } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 
 const SafeAreaView = styled(RNSafeAreaView)
 
 export default function App() {
+  const tabBarHeight = useBottomTabBarHeight()
   return (
-    <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-success">
+    <SafeAreaView className="flex-1 bg-background p-5" style={{ paddingBottom: tabBarHeight }}>
+      <Text className="text-7xl font-bold text-success">
+        Welcome to Nativewind!
+      </Text>
+      <Text className="text-7xl font-sans-extrabold text-success">
         Welcome to Nativewind!
       </Text>
       <Link href="/Onboarding" className="text-xl font-bold text-success bg-primary p-4 rounded-lg">Go to  Onboarding</Link>
